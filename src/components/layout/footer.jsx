af4573e156bf03594164fa1bff4fedccd9d60f12
Guilherme Garcia <guilherme.garcia86@gmail.com>
2016-10-18 19:27:36 -0200

    Alterado css para corrigir o rodape da pagina

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -8,7 +8,6 @@ const Footer = React.createClass({
 
             <footer id="myFooter">
                 <div className="w3-container w3-theme-l2 w3-padding-32">
-                <h4>Footer</h4>
                 </div>
 
                 <div className="w3-container w3-theme-l1">
